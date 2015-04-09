3a9101a0e22419a2dfd5f7ec64b82eaef2c4547e
Daniel Bünzli <daniel.buenzli@erratique.ch>
2015-04-09 15:01:02 +0200

    Improve win32 build support.

--- pkg/build.ml
+++ pkg/build.ml
@@ -19,8 +19,9 @@ let () =
     Pkg.lib "pkg/META";
     Pkg.lib ~exts:Exts.module_library "src-os/mtime"
       ~dst:"os/mtime";
-    Pkg.lib ~exts:[".a"] "src-os/libmtime_stubs" ~dst:"os/libmtime_stubs";
-    Pkg.stublibs "src-os/dllmtime_stubs.so";
+    Pkg.lib ~exts:Exts.c_library "src-os/libmtime_stubs"
+      ~dst:"os/libmtime_stubs";
+    Pkg.stublibs ~exts:Exts.c_dll_library "src-os/dllmtime_stubs";
     Pkg.lib ~exts:Exts.library "src-os/mtime_top" ~dst:"os/mtime_top";
     Pkg.lib "src-os/mtime_top_init.ml" ~dst:"os/mtime_top_init.ml";
     Pkg.lib ~cond:jsoo ~exts:Exts.module_library "src-jsoo/mtime"
