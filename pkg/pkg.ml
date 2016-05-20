c633813fd2ea62f9375499eb0c9540e4d3b233d7
Daniel Bünzli <daniel.buenzli@erratique.ch>
2016-05-20 18:36:57 +0200

    Adapt to latest topkg.

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -4,13 +4,14 @@
 open Topkg
 
 let jsoo = Conf.with_pkg "js_of_ocaml"
+
 let () =
   Pkg.describe "mtime" @@ fun c ->
   let jsoo = Conf.value c jsoo in
-  Ok [ Pkg.mllib ~api:["Mtime"] "src-os/mtime.mllib" ~dst_dir:"os";
-       Pkg.lib ~exts:Exts.c_library "src-os/libmtime_stubs" ~dst:"os/libmtime_stubs";
+  Ok [ Pkg.mllib ~api:["Mtime"] "src-os/mtime.mllib" ~dst_dir:"os/";
+       Pkg.lib ~exts:Exts.c_library "src-os/libmtime_stubs" ~dst:"os/";
        Pkg.stublibs ~exts:Exts.c_dll_library "src-os/dllmtime_stubs";
-       Pkg.lib "src-os/mtime_top_init.ml" ~dst:"os/mtime_top_init.ml";
+       Pkg.lib "src-os/mtime_top_init.ml" ~dst:"os/";
        Pkg.mllib ~cond:jsoo ~api:["Mtime"] "src-jsoo/mtime.mllib"
          ~dst_dir:"jsoo";
        Pkg.doc "test-os/min.ml" ]
