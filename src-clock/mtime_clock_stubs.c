c6649d950a0df56f692a68ecc9053cdaea517320
Anarchos <sylvain_kerjean@hotmail.com>
2022-05-05 15:03:47 +0000

    Haiku

--- src-clock/mtime_clock_stubs.c
+++ src-clock/mtime_clock_stubs.c
@@ -20,7 +20,7 @@
 #if defined(__APPLE__) && defined(__MACH__)
   #define OCAML_MTIME_DARWIN
 
-#elif defined(__unix__) || defined(__unix)
+#elif defined(__unix__) || defined(__unix) || defined(__HAIKU__)
  #include <unistd.h>
  #if defined(_POSIX_VERSION)
    #define OCAML_MTIME_POSIX
